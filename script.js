67b90786e295bd0a61c9fc38d6b2bf7f86c3b70a
SimranGrewal-123 <154738219+SimranGrewal-123@users.noreply.github.com>
2026-01-01 16:04:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function updateCart() {
     cartItems.innerHTML = "";
     cart.forEach(item => {
         const li = document.createElement("li");
-        li.textContent = `${item.productName} - $CAD${item.price}`;
+        li.textContent = `${item.productName} - CAD$${item.price}`;
         cartItems.appendChild(li);
     });
 
